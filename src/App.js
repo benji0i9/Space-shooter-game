90d721ced713f761d9e63ceb711427c0575f799a
jeffrey hernandez <hernandezjeffrey2510@gmail.com>
2022-10-20 18:46:34 -0700

    added new onKeyDown

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 import spaceship from './assets/Galaga_Fighter.png'
 import { useState } from 'react';
 function App() {
 
   const [movement, setMovement] = useState("2px solid black");
-    function handleKeyPress(e) {
-        var key = e.key;
-        console.log( "You pressed a key: " + key );
-        if (key == 37) {
-           console.log("you clicked left")
-        }
-        else if (key == 39) {
-            console.log("you clicked right")
-        }
-    }
+  // function handleKeyPress(e) {
+  //   var key = e.key;
+  //   console.log("You pressed a key: " + key);
+  //   if (key == 37) {
+  //     console.log("you clicked left")
+  //   }
+  //   else if (key == 39) {
+  //     console.log("you clicked right")
+  //   }
+  // }
+  const handleKeyDown = event => {
+    console.log('User pressed: ', event.key);
+  };
 
   return (
     <div className="">
-      <img onKeyDown={handleKeyPress} src={spaceship}/>
+      <div tabIndex={0} onKeyDown={handleKeyDown}>
+        <img src={spaceship} />
+      </div>
 
     </div>
   );
