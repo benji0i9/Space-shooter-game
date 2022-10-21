cb04620310701bd8df97eb193750663aba66384d
TXT-27 <txt-27@MacBook-Pro.attlocal.net>
2022-10-20 20:01:49 -0700

    Added Borders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
 import spaceship from "./assets/Galaga_Fighter.png";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default (App) => {
+  const [marginLeft, setMarginLeft] = useState(0);
   useEffect(() => {
     function handleKeyDown(e) {
       console.log(e.keyCode);
       if (e.keyCode == 37) {
         console.log("1");
+      } else if (e.keyCode == 39) {
+        console.log("Right");
+        if (marginLeft < window.outerWidth - 75) {
+          setMarginLeft(marginLeft + 75);
+        } else {
+          const value = window.outerWidth - 75;
+          setMarginLeft(value);
+        }
       }
     }
 
@@ -17,14 +26,24 @@ export default (App) => {
     return function cleanup() {
       document.removeEventListener("keydown", handleKeyDown);
     };
-  }, []);
+  }, [marginLeft]);
+  console.log(window.innerWidth);
   return (
     <div className="">
-      <div tabIndex={0} onKeyDown={handleKeyDown}>
-        <img src={spaceship} />
+      <div className="App" id="animatedBackground">
+        <div>
+          <img
+            src={spaceship}
+            style={{
+              marginLeft: marginLeft,
+              width: 75,
+              borderWidth: 1,
+              borderStyle: "solid",
+              borderColor: "white",
+            }}
+          />
+        </div>
       </div>
-
-      <div className="App" id="animatedBackground"></div>
     </div>
   );
 };
