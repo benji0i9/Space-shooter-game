85511f484795ad000c389c4d93a8d30bcc0d3969
Roberto sanchez <robsanchez1772@gmail.com>
2022-10-22 14:13:35 -0700

    rob-missile-refactor

---
+++ src/components/Missile/Missile.js
@@ -0,0 +1,32 @@
+import missile from "../../assets/missile.png";
+import { useState, useEffect } from "react";
+
+export default function Missile(props) {
+  console.log(props);
+  const { left, height, width } = props;
+  // tracks the missles y position
+  const [bottom, setBottom] = useState(300 + 80); // adds spaceship height
+
+  /**
+   * This handles the y movement of the all the missiles
+   */
+  useEffect(() => {
+    setTimeout(() => {
+      setBottom(bottom + 5);
+    }, 1000);
+  }, [bottom]);
+
+  return (
+    <img
+      alt="bullet"
+      src={missile}
+      style={{
+        position: "absolute",
+        bottom,
+        left,
+        width,
+        height,
+      }}
+    />
+  );
+}
