3129bfe6b3121aa6eb5b1854888d72e91f88c9c4
Angelo_Hipona <drsagaming@gmail.com>
2022-10-21 22:52:32 -0700

    specshep

---
+++ src/components/SpaceShip.js
@@ -0,0 +1,59 @@
+import spaceship from '../assets/spaceship.png'
+import React, { useEffect, useState } from "react";
+
+function SpaceShip() {
+    const [marginLeft, setMarginLeft] = useState(0);
+    useEffect(() => {
+      function handleKeyDown(e) {
+        console.log(e.keyCode);
+        //Left Arrow Key
+        if (e.keyCode === 37) {
+          console.log("Left");
+          if (marginLeft > 0) {
+            setMarginLeft(marginLeft - 75);
+          } else {
+            setMarginLeft(0);
+          }
+          //Right Arrow Key
+        } else if (e.keyCode === 39) {
+          console.log("Right");
+          if (marginLeft < window.outerWidth - 75) {
+            setMarginLeft(marginLeft + 75);
+          } else {
+            const value = window.outerWidth - 75;
+            setMarginLeft(value);
+          }
+        }
+        // Spacebar
+        if (e.keyCode === 32) {
+          console.log("Spacebar!");
+        }
+      }
+
+      document.addEventListener("keydown", handleKeyDown);
+
+      // Don't forget to clean up
+      return function cleanup() {
+        document.removeEventListener("keydown", handleKeyDown);
+      };
+    }, [marginLeft]);
+    console.log(marginLeft);
+    console.log(window.innerWidth);
+return(
+    <div>
+        <img
+            alt="Spaceship"
+            src={spaceship}
+            style={{
+                marginLeft: marginLeft,
+                width: 75,
+                alignSelf: "center",
+                flex: 1,
+                marginTop: window.outerHeight / 2,
+            }}
+          />
+    </div>
+)
+}
+
+export default SpaceShip;
