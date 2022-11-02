da57371c06145c4d478e5f786f8ed508e4331632
rob <robsanchez1772@gmail.com>
2022-11-01 18:10:46 -0700

    added routing;added api req

--- src/App.js
+++ src/App.js
@@ -1,126 +1,23 @@
-
-import './App.css'
-import spaceship from './assets/spaceship.png'
-import missile from './assets/missile.png'
-
 import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
+import Game from "./pages/Game/Game";
+import HighScores from "./pages/HighScores/HighScores";
 
-import React, { useState } from "react";
-import Timer from './components/Timer/Timer'
-import SpaceShip from './components/SpaceShip/SpaceShip'
-
-
-
-
+/**
+ * Renders react router
+ * @returns
+ */
 export default function App() {
-  const [movement, setMovement] = useState('2px solid black')
-
-  // let img = new Image();
-
-  // function handleKeyUp(e) {
-
-  //   if (e.keyCode === 32) {
-  //     console.log("Spacebar!");
-  //     img.src = missile;
-  //     img.classList.add('missileImage')
-  //     img.width = 50;
-  //     img.height = 50;
-  //     img.marginLeft = marginLeft;
-  //     document.getElementById("Body").appendChild(img);
-  //   }
-
-  // }
-
-  // image that holds image true or false
-
-  // if space -> make the use effect 
-  // get spaceship margin location to render the laser
-
-
-
-  const handleKeyDown = (event) => {
-    console.log('User pressed: ', event.key)
-  }
-
-
-  const [marginLeft, setMarginLeft] = useState(0);
-  // useEffect(() => {
-  //   function handleKeyDown(e) {
-  //     console.log(e.keyCode);
-  //     //Left Arrow Key
-  //     if (e.keyCode === 37) {
-  //       console.log("Left");
-  //       if (marginLeft > 0) {
-  //         setMarginLeft(marginLeft - 75);
-  //       } else {
-  //         setMarginLeft(0);
-  //       }
-  //       //Right Arrow Key
-  //     } else if (e.keyCode === 39) {
-  //       console.log("Right");
-  //       if (marginLeft < window.outerWidth - 75) {
-  //         setMarginLeft(marginLeft + 75);
-  //       } else {
-  //         const value = window.outerWidth - 75;
-  //         setMarginLeft(value);
-  //       }
-  //     }
-  //     // Spacebar
-  //     if (e.keyCode === 32) {
-  //       console.log("Spacebar!");
-  //     }
-  //   }
-
-  //   document.addEventListener("keydown", handleKeyDown);
-
-  //   document.addEventListener("keydown", handleKeyUp);
-
-  //   // Don't forget to clean up
-  //   return function cleanup() {
-  //     document.removeEventListener("keydown", handleKeyDown);
-  //   };
-  // }, [marginLeft]);
-  console.log(marginLeft);
-  console.log(window.innerWidth);
-
-
-
-
-
   return (
-    
-
-      
-
-     
-
-      <div className="App" id="animatedBackground">
-        <div>
-          
-          <Timer />
-
-         {/* <div id="spacedivboom">
-          <img
-              id='spaceshipdiv'
-              alt="Spaceship"
-              src={spaceship}
-              style={{
-                marginLeft: marginLeft,
-                width: 75,
-                borderWidth: 1,
-                borderStyle: "solid",
-                borderColor: "white",
-                marginTop: window.outerHeight / 2,
-              }}
-            />
-         </div> */}
-          
-
-
-          <SpaceShip />
-
-        </div>
-      </div>
-
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Game />} />
+          <Route path="/hi-scores" element={<HighScores />} />
+          <Route path="*" element={<Navigate to="" />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+// this is the url for our server
+export const API_URL = 'http://localhost:4020';
